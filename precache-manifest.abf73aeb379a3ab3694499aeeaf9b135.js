410534250742649c8ce2904f96105aaa502d6421
Hugo Encinas <hugomty93@outlook.com>
2020-02-03 23:55:01 -0600

    Updates

---
+++ precache-manifest.abf73aeb379a3ab3694499aeeaf9b135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de4016477d2f553b319b553ce5f4a59",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "e2574e818ec3a7aeb29d",
+    "url": "/react-news-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2d7f6f610cd5e67351e8",
+    "url": "/react-news-app/static/css/main.a7e1227e.chunk.css"
+  },
+  {
+    "revision": "e2574e818ec3a7aeb29d",
+    "url": "/react-news-app/static/js/2.ed3d1631.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/react-news-app/static/js/2.ed3d1631.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d7f6f610cd5e67351e8",
+    "url": "/react-news-app/static/js/main.8a802141.chunk.js"
+  },
+  {
+    "revision": "dc4a3639f6f67af2405d",
+    "url": "/react-news-app/static/js/runtime-main.ab8833fe.js"
+  }
+]);
